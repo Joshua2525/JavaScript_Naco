adb55e1a70c238e3a86d038bf584dfeac891dfb5
Joshua Naco <69311400+Joshua2525@users.noreply.github.com>
2023-05-17 21:37:24 +0800

    jsFunction.js

--- jsFunction.js
+++ jsFunction.js
@@ -1,21 +1,20 @@
 function artistAge() {
-    // User enter the age of their favorite artist
-    var age = prompt("Can you please enter the age of your favorite artist?");
-  
-    // This will be the output if the age is less than 25
-    if (age < 25) {
-      console.log("They’re " + age + " years old and they’re younger than Google");
-    }
-    // This will be the output if the age is equal to 25
-    else if (age === "25") {
-      console.log("They’re " + age + " years old and they’re as old as Google");
-    }
-    // This will be the output if the age greater than 25
-    else {
-      alert("They’re " + age + " years old and they’re older than Google");
-    }
+  // User enters the age of their favorite artist
+  var age = prompt("Can you please enter the age of your favorite artist?");
+
+  // This will be the output if the age is less than 25
+  if (parseInt(age) < 25) {
+    document.write("They're " + age + " years old and they're younger than Google");
   }
-  
-  // Call the artistAge function
-  artistAge();
-  
+  // This will be the output if the age is equal to 25
+  else if (parseInt(age) === 25) {
+    document.write("They're " + age + " years old and they're as old as Google");
+  }
+  // This will be the output if the age is greater than 25
+  else {
+    alert("They're " + age + " years old and they're older than Google");
+  }
+}
+
+// Call the artistAge function
+artistAge();
